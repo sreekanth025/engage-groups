02c85dad57b9d08d68fd5a831715fa9f5690d209
sreekanth025 <vs25@iitbbs.ac.in>
2021-11-14 14:11:11 +0530

    Basic JWT implementation

--- src/main/java/com/sreekanth/engage/controllers/HelloController.java
+++ src/main/java/com/sreekanth/engage/controllers/HelloController.java
@@ -1,13 +1,23 @@
 package com.sreekanth.engage.controllers;
 
+import com.sreekanth.engage.config.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
     @RequestMapping("/hello")
-    public String sayHello() {
+    public String sayHello(@RequestHeader("Authorization") String auth) {
+        String token = auth.substring(7);
+        String username = jwtTokenUtil.getUsernameFromToken(token);
+        System.out.println(username);
+
         return "Hello Engage";
     }
 }
